8ca7595b2b853ee3d21f8199fa780e2dcb6da5cd
Sirpip91 <27028592+Sirpip91@users.noreply.github.com>
2024-06-07 15:10:50 -0500

    setup blog pages

---
+++ app/blog/page.tsx
@@ -0,0 +1,46 @@
+import {posts} from "#site/content";
+import { PostItem } from "../../components/post-items";
+import {sortPosts} from "@/lib/utils";
+
+export default async function BlogPage(){
+
+    const sortedPosts = sortPosts(posts.filter(post=>post.published));
+
+    const displayPosts = posts;
+    
+    return (
+    <div className="container max-w-4xl py-6 lg:py10">
+        <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+            <div className="flex-1 space-y-4">
+                <h1 className="inline-block font-black text-4xl lg:text-5xl">
+                    Blog
+                </h1>
+                <p className="text-xl text-muted-forground">
+                    My aweseom blog thingy with react and next.
+                </p>
+            </div>
+        </div>
+        <hr className="mt-8"/>
+        {displayPosts?.length > 0 ? (
+            <ul className="flex flex-col">
+                {displayPosts.map((post)=> {
+                        const {slug,date,title,description}  = post;
+                        return (
+                            <li key = {slug}>
+                                <PostItem 
+                                    slug = {slug}
+                                    date = {date}
+                                    title = {title}
+                                    description= {description}
+                                />
+                            </li>
+                        );
+                    }
+                )}
+            </ul>
+        ) : (
+            <p>Nothing to see here yet</p>
+        )}
+    </div>
+    );
+}
