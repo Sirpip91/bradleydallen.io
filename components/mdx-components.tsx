a0c90250e23974a7d243d78e96ea47045c060c88
Sirpip91 <27028592+Sirpip91@users.noreply.github.com>
2024-06-07 16:19:45 -0500

    custom components

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import * as runtime from "react/jsx-runtime";
 import { cn } from "@/lib/utils";
+import { Callout } from "./callout";
 
 const useMDXComponent = (code: string) => {
   const fn = new Function(code);
@@ -9,11 +10,13 @@ const useMDXComponent = (code: string) => {
 
 const components = {
   Image,
+  Callout,
   
 };
 
 interface MdxProps {
   code: string;
+
 }
 
 export function MDXContent({ code }: MdxProps) {
