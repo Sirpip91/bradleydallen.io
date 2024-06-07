960b7552ba956b747c3b8e636c01ad3a7764604b
Sirpip91 <27028592+Sirpip91@users.noreply.github.com>
2024-06-07 12:42:39 -0500

    Install UI Libary

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+//bg-blue-500 bg-red-5--
