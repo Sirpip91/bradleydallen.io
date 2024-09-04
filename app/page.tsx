e4db875848473c1d4e695e6c44ce5aeebfa03962
sirpip91 <27028592+Sirpip91@users.noreply.github.com>
2024-09-03 21:24:50 -0500

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       </section>
       <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
         <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
-          Latest Posts
+         Latest Content
         </h2>
         <ul className="flex flex-col">
           {latestPosts.map((post) => (
