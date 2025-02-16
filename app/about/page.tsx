3e08ada0efa6e37e4a585fa13d73dfdfe80ae04c
Sirpip91 <27028592+Sirpip91@users.noreply.github.com>
2025-02-16 17:42:52 -0600

    Update about me Picture

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,7 +21,7 @@ export default async function AboutPage() {
       <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
         <div className="min-w-48 max-w-48 flex flex-col gap-2">
           <Avatar className="h-48 w-48">
-            <AvatarImage src="https://yt3.ggpht.com/GKvoDZysnAYSZBz4amIOhatls1uczmsYx8-FfmGeMN9fhQSo-oFPh_nGMZwzXHLNO3mJ5OcKVwI=s108-c-k-c0x00ffffff-no-rj" alt={siteConfig.author} />
+          <AvatarImage src="/static/profile.png" alt={siteConfig.author} />
             <AvatarFallback>BA</AvatarFallback>
           </Avatar>
           <h2 className="text-2xl font-bold text-center break-words">
